00e5d74e90ea1994983d002b42f43a6761f1915c
이준경 <jg.lee@piolink.com>
2025-06-18 08:22:45 +0900

    add Code Companion (AI agent)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.winbar = "%=%m %f"
+require("noice").setup({
+  cmdline = {
+    view = "cmdline",
+  },
+})
--- lua/plugins/gemini.lua
+++
@@ -1,6 +0,0 @@
-return {
-  {
-    'kiddos/gemini.nvim',
-    opts = {}
-  }
-}
